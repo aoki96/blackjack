373c424dc554185ede8b6d3d5f59d0c455a655e3
aoki96 <146246082+aoki96@users.noreply.github.com>
2024-01-22 16:29:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
   const playerNameInput = document.getElementById('playerName');
 
   function launchGame() {
-    debugger;
     const playerName = playerNameInput.value.trim();
     if (playerName !== '') {
-      console.log(window.location.href);
       sessionStorage.setItem('playerName', playerName);
-      window.location.href = window.location.href + 'game.html';
+      window.location.href = 'game.html';
     } else {
       alert('Please enter your name.');
   }
